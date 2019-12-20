bfac8149d0e8097a9e11ac650dc01ce5e2569d9b
Jimmer942 <942@holbertonschool.com>
2019-12-20 16:41:18 -0500

    user.py

--- models/user.py
+++ models/user.py
@@ -11,7 +11,10 @@ class User(BaseModel):
         first_name: first name
         last_name: last name
     """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship('Place', backref='user')
+    reviews = relationship('Review', backref='user')
+    __tablename__ = 'users'
