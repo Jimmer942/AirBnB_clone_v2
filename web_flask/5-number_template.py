8f41c0dcd7f338168cc76f3e0e50c8b5f51a9b57
Jimmer942 <942@holbertonschool.com>
2020-01-20 16:56:57 -0500

    5-number_template.py

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+"""script that starts a Flask web application"""
+
+
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """ Hello HBNB """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ HBNB """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C(text):
+    """ C ... """
+    return "C " + text.replace('_', ' ')
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def Python(text='is cool'):
+    """ Python ... """
+    return "Python " + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """ n is a number"""
+    if type(n) is int:
+        return str(n) + " is a number"
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """ template """
+    if type(n) is int:
+        return render_template('5-number.html', number=n)
+
+if __name__ == '__main__':
+    app.run()
